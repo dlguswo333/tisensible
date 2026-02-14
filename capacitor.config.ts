e1f1088f014a8657f6533ddd9bc98ebfa669ee6b
dlguswo333 <dlguswo002@gmail.com>
2026-02-14 19:32:39 +0900

    Install capacitor core runtime

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type {CapacitorConfig} from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.dlguswo333.tisensible',
+  appName: 'tiSensible',
+  webDir: 'dist',
+};
+
+export default config;
