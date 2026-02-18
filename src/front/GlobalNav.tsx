412e0141d4ae93f07ee135930b58b92d5306e97a
dlguswo333 <dlguswo002@gmail.com>
2026-02-18 19:01:49 +0900

    Add active button style in GlobalNav

--- src/front/GlobalNav.tsx
+++ src/front/GlobalNav.tsx
@@ -1,29 +1,28 @@
-import {useNavigate} from 'react-router';
+import {NavLink} from 'react-router';
 
 type ButtonProps = {
   text: string;
-  onClick: () => unknown;
+  to: string;
+  replace: boolean;
 };
 
-const Button = ({text, onClick}: ButtonProps) => {
+const Button = ({text, to, replace}: ButtonProps) => {
   return (
-    <button
-      type='button'
-      onClick={onClick}
-      className='border-gray-400 dark:border-gray-600 text-black dark:text-white not-last:border-r px-2 py-1 text-[0.7rem] active:bg-sky-300/50 transition-colors'
+    <NavLink
+      to={to}
+      replace={replace}
+      className='border-gray-400 dark:border-gray-600 text-black dark:text-white not-last:border-r px-2 py-1 text-[0.7rem] active:bg-sky-300/50 [&.active]:bg-sky-300/50 transition-colors'
     >
       {text}
-    </button>
+    </NavLink>
   );
 };
 const GlobalNav = () => {
-  const navigate = useNavigate();
-
   return (
     <nav className='absolute overflow-hidden bottom-5 self-center flex flex-row border-gray-400 dark:border-gray-600 bg-white dark:bg-gray-800 border rounded-sm'>
-      <Button text='Compass' onClick={() => navigate('/compass', {replace: true})} />
-      <Button text='Speedometer' onClick={() => navigate('/speedometer', {replace: true})} />
-      <Button text='Settings' onClick={() => navigate('/settings', {replace: false})} />
+      <Button text='Compass' to='/compass' replace={true} />
+      <Button text='Speedometer' to='/speedometer' replace={true} />
+      <Button text='Settings' to='/settings' replace={false} />
     </nav>
   );
 };
