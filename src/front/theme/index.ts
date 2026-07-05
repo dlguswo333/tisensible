4a3c1b01421ed71c9dd635860108c9e164195a26
dlguswo333 <dlguswo002@gmail.com>
2026-07-05 18:04:47 +0900

    [#7] Support edge-to-edge

--- src/front/theme/index.ts
+++ src/front/theme/index.ts
@@ -1,3 +1,4 @@
+import {SystemBars, SystemBarsStyle} from '@capacitor/core';
 import {atomWithDefault} from 'jotai/utils';
 import z from 'zod';
 
@@ -18,7 +19,9 @@ export const updateTheme = (newValue: z.infer<typeof themeType>) => {
   localStorage.setItem(THEME_STORE_KEY, newValue);
   if (newValue === 'dark' || (newValue === 'system' && preferDarkMediaQuery.matches)) {
     document.body.classList.add('dark');
+    SystemBars.setStyle({style: SystemBarsStyle.Dark});
   } else {
     document.body.classList.remove('dark');
+    SystemBars.setStyle({style: SystemBarsStyle.Light});
   }
 };
--- src/front/theme/useSyncTheme.ts
+++ src/front/theme/useSyncTheme.ts
@@ -1,3 +1,4 @@
+import {SystemBars, SystemBarsStyle} from '@capacitor/core';
 import {useAtomValue} from 'jotai';
 import {useEffect} from 'react';
 import {preferDarkMediaQuery, themeAtom} from '.';
@@ -6,20 +7,20 @@ const useSyncTheme = () => {
   const currentTheme = useAtomValue(themeAtom);
 
   useEffect(() => {
-    const onChange = (event: MediaQueryListEvent) => {
-      if (currentTheme !== 'system') {
-        return;
-      }
-      if (event.matches) {
+    const syncTheme = (event: MediaQueryListEvent | MediaQueryList) => {
+      if (currentTheme === 'dark' || (currentTheme === 'system' && event.matches)) {
         document.body.classList.add('dark');
+        SystemBars.setStyle({style: SystemBarsStyle.Dark});
       } else {
         document.body.classList.remove('dark');
+        SystemBars.setStyle({style: SystemBarsStyle.Light});
       }
     };
-    preferDarkMediaQuery.addEventListener('change', onChange);
 
+    syncTheme(preferDarkMediaQuery);
+    preferDarkMediaQuery.addEventListener('change', syncTheme);
     return () => {
-      preferDarkMediaQuery.removeEventListener('change', onChange);
+      preferDarkMediaQuery.removeEventListener('change', syncTheme);
     };
   }, [currentTheme]);
 };
