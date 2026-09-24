1707302f4585edc821f285536e3adfd73315f3b3
dlguswo333 <dlguswo002@gmail.com>
2026-09-24 19:31:02 +0900

    [#14] Fix NavBar having different sizes on different pages

--- src/front/navBar/NavBar.tsx
+++ src/front/navBar/NavBar.tsx
@@ -18,7 +18,7 @@ const Button = ({text, icon, to, replace}: ButtonProps) => {
       to={to}
       draggable={false}
       replace={replace}
-      className='border-gray-400 dark:border-gray-600 text-black dark:text-white not-last:border-r min-w-15 px-5 first:pl-6 last:pr-6 py-3 flex flex-col items-center justify-center text-[0.7rem] active:bg-sky-300/50 [&.active]:bg-sky-300/50 transition-colors'
+      className='border-gray-400 dark:border-gray-600 text-black dark:text-white not-last:border-r min-w-15 px-5 first:pl-6 last:pr-6 py-1.5 flex flex-col items-center justify-center text-[0.7rem] active:bg-sky-300/50 [&.active]:bg-sky-300/50 transition-colors'
     >
       <div className='size-7.5 grid place-items-center'>{icon}</div>
       {text}
@@ -30,7 +30,7 @@ const NavBar = memo(() => {
   const {t} = useTranslation();
 
   return (
-    <nav className='sticky mt-4 mb-8 bottom-8 self-center border-gray-400 dark:border-gray-600 bg-white dark:bg-gray-800 shadow-sm shadow-gray-600/50 dark:shadow-black/50 border rounded-4xl overflow-hidden flex'>
+    <nav className='shrink-0 sticky mt-4 mb-8 bottom-8 self-center border-gray-400 dark:border-gray-600 bg-white dark:bg-gray-800 shadow-sm shadow-gray-600/50 dark:shadow-black/50 border rounded-4xl overflow-hidden flex'>
       <div className='flex flex-row'>
         <Button text={t('navBar.compass')} icon={<CompassIcon />} to='/compass' replace={true} />
         <Button text={t('navBar.speedometer')} icon={<SpeedometerIcon />} to='/speedometer' replace={true} />
